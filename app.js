b941c362cbe3b87a4ff0ed127ce5e4ca7909c2ef
programmerKFH <kfh.freelancer@gmail.com>
2024-01-24 23:59:42 -0400

    Ejercicios del modulo 1 del curso logica de programacion con javascript impartido por Alura Latam.

---
+++ app.js
@@ -0,0 +1,7 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt('Me indicas un numero por favor: ');
+console.log(numeroUsuario);
+
+if (numeroSecreto == numeroUsuario) {
+    console.log('Adivinaste el numero secreto');
+}
---
+++ ejercicios_unidad_1/app.js
@@ -0,0 +1,32 @@
+//Desafio 1
+alert('!Bienvenida y bienvenido a nuestro sitio web!');
+
+//Desafio 2
+let nombre = 'Luna';
+
+//Desafio 3
+let edad = 25;
+
+//Desafio 4
+let numeroDeVentas = 50;
+
+//Desafio 5
+let saldoDisponible = 1000;
+
+//Desafio 6
+alert('!Error! Completa todos los campos');
+
+//Desafio 7
+let mensajeDeError = '!Error! Completa todos los campos';
+alert(mensajeDeError);
+
+//Desafio 8
+nombre = prompt('Cual es tu nombre?');
+
+//Desafio 9
+edad = prompt('Ingrese su edad');
+
+//Desafio 10
+if (edad >= 18){
+    alert('!Puedes obtener tu licencia de conducir!');
+}
