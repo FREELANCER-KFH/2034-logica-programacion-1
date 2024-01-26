41943bb006782fac5c04d46fafe6020f51cb14c5
programmerKFH <kfh.freelancer@gmail.com>
2024-01-25 22:54:30 -0400

    Actualizacion del juego 'Adivina el numero secreto'.

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-let numeroSecreto = 5;
+let numeroSecreto = Math.floor(Math.random() * 10) + 1
 let numeroUsuario = 0;
 let intentos = 1;
-let palabra = ' vez';
+//let palabra = ' vez';
+let maximosIntentos = 3;
+
+alert('Bienvenido al juego de adivinar el numero secreto');
+alert('Tienes 3 intentos para adivinar el numero secreto');
 
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = prompt('Ingrese un numero entre 1 y 10');
+    numeroUsuario = parseInt(prompt('Ingrese un numero entre 1 y 10'));
+    console.log(typeof numeroUsuario);
     /*
 Este codigo realiza la comparacion entre el numero secreto y el numero que el usuario ingreso
 */
     if (numeroSecreto == numeroUsuario) {
         //Codigo que se ejecuta si la condicion es verdadera
         console.log('Adivinaste el numero secreto');
-        alert(`Adivinaste el numero secreto ${numeroSecreto}. Intentaste ${intentos +  palabra}`);
+        alert(`Adivinaste el numero secreto ${numeroSecreto}. Intentaste ${intentos} ${intentos > 1 ? 'veces' : 'vez'}`);
     } else {
         //Codigo que se ejecuta si la condicion es falsa
         if (numeroUsuario < numeroSecreto) {
@@ -20,6 +25,9 @@ Este codigo realiza la comparacion entre el numero secreto y el numero que el us
             alert('El numero que ingresaste es mayor al numero secreto');
         }
         intentos++;
-        palabra = ' veces'
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al maximo numero de intentos que son ${maximosIntentos}. El numero secreto era ${numeroSecreto}`);
+            break;
+        }
     }
 }
