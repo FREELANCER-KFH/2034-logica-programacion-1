b632200e2425fc5ac7a7838a90d7844af9d35568
programmerKFH <kfh.freelancer@gmail.com>
2024-01-25 22:06:36 -0400

    Ejercicios de la unidad 3.

---
+++ ejercicios_unidad_3/app.js
@@ -0,0 +1,34 @@
+// Desafio 1
+
+let contador = 1;
+
+while (contador <= 10){
+    console.log(contador);
+    contador++;
+}
+
+// Desafio 2
+
+contador = 10;
+ while (contador >= 0){
+     console.log(contador);
+     contador--;
+ }
+
+// Desafio 3
+
+contador = prompt("Ingrese un numero");
+iterador = 0;
+while (iterador <= contador){
+    console.log(iterador);
+    iterador++;
+}
+
+// Desafio 4
+
+contador = prompt("Ingrese un numero");
+iterador = 0;
+while (contador >= iterador){
+    console.log(iterador);
+    iterador++;
+}
